646f0fe6a4526e5d2c8e76e89f772dd35a2fd2de
Julien Desfossez <jdesfossez@efficios.com>
2014-02-21 13:47:32 -0500

    per-cpu computing and split in functions
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- sched_switch.py
+++ sched_switch.py
@@ -13,45 +13,96 @@
 import sys
 from babeltrace import *
 
+NSEC_PER_SEC = 1000000000
+
 class Process():
     def __init__(self):
         pass
 
 class CPU():
+    def __init__(self):
+        pass
+
+class CPUAnalyzes():
     def __init__(self, traces):
         self.start_ts = 0
         self.end_ts = 0
         self.traces = traces
         self.tids = {}
+        self.cpus = {}
+
+    def per_cpu(self, cpu_id, ts, next_tid):
+        """Compute per-cpu usage"""
+        if self.cpus.has_key(cpu_id):
+            c = self.cpus[cpu_id]
+            if c.start_ns != 0:
+                c.cpu_ns += ts - c.start_ns
+            # exclude swapper process
+            if next_tid != 0:
+                c.start_ns = ts
+            else:
+                c.start_ns = 0
+        else:
+            c = CPU()
+            c.cpu_ns = 0
+            c.start_ns = ts
+            self.cpus[cpu_id] = c
+
+    def per_tid(self, ts, prev_tid, next_tid, next_comm):
+        """Compute per-tid usage"""
+        # per-tid usage
+        if self.tids.has_key(prev_tid):
+            p = self.tids[prev_tid]
+            p.cpu_ns += (ts - p.last_sched)
+
+        if not self.tids.has_key(next_tid):
+            p = Process()
+            p.tid = next_tid
+            p.comm = next_comm
+            p.cpu_ns = 0
+            self.tids[next_tid] = p
+        else:
+            p = self.tids[next_tid]
+        p.last_sched = ts
+
+    def sched_switch(self, event):
+        """Handle sched_switch event"""
+        prev_tid = event["prev_tid"]
+        next_comm = event["next_comm"]
+        next_tid = event["next_tid"]
+        cpu_id = event["cpu_id"]
+
+        self.per_cpu(cpu_id, event.timestamp, next_tid)
+        self.per_tid(event.timestamp, prev_tid, next_tid, next_comm)
 
     def run(self):
+        """Process the trace"""
         for event in self.traces.events:
             if self.start_ts == 0:
                 self.start_ts = event.timestamp
             self.end_ts = event.timestamp
 
-            if event.name != "sched_switch":
-                continue
+            if event.name == "sched_switch":
+                self.sched_switch(event)
 
-            prev_tid = event["prev_tid"]
-            next_comm = event["next_comm"]
-            next_tid = event["next_tid"]
-
-            if self.tids.has_key(prev_tid):
-                p = self.tids[prev_tid]
-                p.cpu_ns += (event.timestamp - p.last_sched)
-
-            if not self.tids.has_key(next_tid):
-                p = Process()
-                p.tid = next_tid
-                p.comm = next_comm
-                p.cpu_ns = 0
-                self.tids[next_tid] = p
-            else:
-                p = self.tids[next_tid]
-            p.last_sched = event.timestamp
-        
+    def text_report(self):
         total_ns = self.end_ts - self.start_ts
+        print("### Trace info ###")
+        print("Start : %lu\nEnd: %lu" % (self.start_ts, self.end_ts))
+        print("Total ns : %lu" % (total_ns))
+        print("Total : %lu.%0.09lus\n" % (total_ns / NSEC_PER_SEC,
+            total_ns % NSEC_PER_SEC))
+        print("### Per-CPU Usage ###")
+        total_cpu_pc = 0
+        for cpu in self.cpus.keys():
+            cpu_total_ns = self.cpus[cpu].cpu_ns
+            cpu_pc = (cpu_total_ns * 100)/total_ns
+            total_cpu_pc += cpu_pc
+            nb_cpu = len(self.cpus.keys())
+            print("CPU %d : %d ns (%0.02f%%)" % (cpu, cpu_total_ns, cpu_pc))
+        print("Total CPU Usage : %0.02f%%" % (total_cpu_pc / nb_cpu))
+
+        print("\n### Per-TID Usage ###")
         for tid in self.tids.keys():
             print("%s (%d) : %0.02f%%" % (self.tids[tid].comm, tid,
                     ((self.tids[tid].cpu_ns * 100)/ total_ns)))
@@ -66,5 +117,6 @@ if __name__ == "__main__":
     if ret is None:
         sys.exit(1)
 
-    c = CPU(traces)
+    c = CPUAnalyzes(traces)
     c.run()
+    c.text_report()
