bb7c408dbd588aab0e2f3e9ee0b000c5feaabe07
Julien Desfossez <jdesfossez@efficios.com>
2014-03-17 19:50:28 -0400

    cputop standalone analysis
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

---
+++ cputop.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+import sys
+import argparse
+import shutil
+import time
+from babeltrace import *
+from LTTngAnalyzes.common import *
+from LTTngAnalyzes.jsonreport import *
+from LTTngAnalyzes.textreport import *
+from LTTngAnalyzes.graphitereport import *
+from LTTngAnalyzes.sched import *
+from LTTngAnalyzes.syscalls import *
+from LTTngAnalyzes.block_bio import *
+from LTTngAnalyzes.net import *
+from LTTngAnalyzes.statedump import *
+from analyzes import *
+from ascii_graph import Pyasciigraph
+
+class CPUTop():
+    def __init__(self, traces):
+        self.trace_start_ts = 0
+        self.trace_end_ts = 0
+        self.traces = traces
+        self.tids = {}
+        self.cpus = {}
+
+    def run(self, args):
+        """Process the trace"""
+        self.current_sec = 0
+        self.start_ns = 0
+        self.end_ns = 0
+
+        sched = Sched(self.cpus, self.tids)
+        for event in self.traces.events:
+            if self.start_ns == 0:
+                self.start_ns = event.timestamp
+            if self.trace_start_ts == 0:
+                self.trace_start_ts = event.timestamp
+            self.end_ns = event.timestamp
+            self.check_refresh(args, event)
+            self.trace_end_ts = event.timestamp
+
+            if event.name == "sched_switch":
+                sched.switch(event)
+        if args.refresh == 0:
+            # stats for the whole trace
+            self.compute_stats()
+            self.output(args, self.trace_start_ts, self.trace_end_ts, final=1)
+        else:
+            # stats only for the last segment
+            self.compute_stats()
+            self.output(args, self.start_ns, self.trace_end_ts,
+                    final=1)
+
+    def check_refresh(self, args, event):
+        """Check if we need to output something"""
+        if args.refresh == 0:
+            return
+        event_sec = event.timestamp / NSEC_PER_SEC
+        if self.current_sec == 0:
+            self.current_sec = event_sec
+        elif self.current_sec != event_sec and \
+                (self.current_sec + args.refresh) <= event_sec:
+            self.compute_stats()
+            self.output(args, self.start_ns, event.timestamp)
+            self.reset_total(event.timestamp)
+            self.current_sec = event_sec
+            self.start_ns = event.timestamp
+
+    def compute_stats(self):
+        for cpu in self.cpus.keys():
+            current_cpu = self.cpus[cpu]
+            total_ns = self.end_ns - self.start_ns
+            if current_cpu.start_task_ns != 0:
+                current_cpu.cpu_ns += self.end_ns - current_cpu.start_task_ns
+            cpu_total_ns = current_cpu.cpu_ns
+            current_cpu.cpu_pc = (cpu_total_ns * 100)/total_ns
+            if current_cpu.current_tid >= 0:
+                self.tids[current_cpu.current_tid].cpu_ns += \
+                    self.end_ns - current_cpu.start_task_ns
+
+    def output(self, args, begin_ns, end_ns, final=0):
+        count = 0
+        limit = args.top
+        total_ns = end_ns - begin_ns
+        graph = Pyasciigraph()
+        values = []
+        for tid in sorted(self.tids.values(),
+                key=operator.attrgetter('cpu_ns'), reverse=True):
+            if len(args.proc_list) > 0 and tid.comm not in args.proc_list:
+                continue
+            pc = float("%0.02f" % ((tid.cpu_ns * 100) / total_ns))
+            values.append(("%s (%d)" % (tid.comm, tid.tid), pc))
+            count = count + 1
+            if limit > 0 and count >= limit:
+                break
+        for line in  graph.graph('%s to %s' % (ns_to_asctime(begin_ns), \
+                ns_to_asctime(end_ns)), values):
+            print(line)
+
+    def reset_total(self, start_ts):
+        for cpu in self.cpus.keys():
+            current_cpu = self.cpus[cpu]
+            current_cpu.cpu_ns = 0
+            if current_cpu.start_task_ns != 0:
+                current_cpu.start_task_ns = start_ts
+            if current_cpu.current_tid >= 0:
+                self.tids[current_cpu.current_tid].last_sched = start_ts
+        for tid in self.tids.keys():
+            self.tids[tid].cpu_ns = 0
+            self.tids[tid].migrate_count = 0
+            self.tids[tid].read = 0
+            self.tids[tid].write = 0
+            for syscall in self.tids[tid].syscalls.keys():
+                self.tids[tid].syscalls[syscall].count = 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='CPU usage analysis')
+    parser.add_argument('path', metavar="<path/to/trace>", help='Trace path')
+    parser.add_argument('-r', '--refresh', type=int,
+            help='Refresh period in seconds', default=0)
+    parser.add_argument('--top', type=int, default=0,
+            help='Limit to top X TIDs')
+    args = parser.parse_args()
+    args.proc_list = []
+
+    traces = TraceCollection()
+    handle = traces.add_trace(args.path, "ctf")
+    if handle is None:
+        sys.exit(1)
+
+    c = CPUTop(traces)
+
+    c.run(args)
+
+    traces.remove_trace(handle)
