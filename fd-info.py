0a976db29e379c5732b8ba478c36891bce6febee
Antoine Busque <antoinebusque@gmail.com>
2014-05-22 15:54:13 -0400

    Add support for read/write events in fd-info.
    Eventually, partial payload should be displayed, as in strace

--- fd-info.py
+++ fd-info.py
@@ -36,19 +36,16 @@ class FDInfo():
         self.open_syscalls = ['sys_open', 'sys_openat', 'sys_dup2',
                               'sys_accept', 'sys_socket', 'sys_fcntl']
         self.close_syscalls = ['sys_close']
-
+        self.read_syscalls = ['sys_read', 'sys_recvmsg', 'sys_recvfrom',
+                              'sys_splice', 'sys_readv', 'sys_sendfile64']
+        self.write_syscalls = ['sys_write', 'sys_sendmsg' 'sys_sendto',
+                               'sys_writev']
 
     def process_event(self, event, sched, syscall, statedump, started=1):
         if event.name == 'sched_switch':
             sched.switch(event)
         elif event.name.startswith('sys_'):
-            if event.name in self.open_syscalls \
-            and self.isOutputEnabled['open']:
-                self.output_open(event)
-            elif event.name in self.close_syscalls \
-            and self.isOutputEnabled['close']:
-                self.output_close(event)
-            syscall.entry(event)
+            self.handle_syscall_entry(event, syscall)
         elif event.name == 'exit_syscall':
             syscall.exit(event, started)
         elif event.name == 'sched_process_fork':
@@ -60,6 +57,21 @@ class FDInfo():
             if self.isOutputEnabled['dump']:
                 self.output_dump(event)
 
+    def handle_syscall_entry(self, event, syscall):
+        if event.name in self.open_syscalls \
+           and self.isOutputEnabled['open']:
+            self.output_open(event)
+        elif event.name in self.close_syscalls \
+             and self.isOutputEnabled['close']:
+            self.output_close(event)
+        elif event.name in self.read_syscalls \
+             and self.isOutputEnabled['read']:
+            self.output_read(event)
+        elif event.name in self.write_syscalls \
+             and self.isOutputEnabled['write']:
+            self.output_write(event)
+
+        syscall.entry(event)
 
     def run(self, args):
         '''Process the trace'''
@@ -101,6 +113,18 @@ class FDInfo():
 
         evt = event.name
 
+        filename = self.compute_open_filename(event)
+        if filename is None:
+            return
+
+        time = ns_to_hour_nsec(event.timestamp)
+        
+        if filename.startswith(self.prefix):
+            print(pid, comm, evt, filename, time)
+
+    def compute_open_filename(self, event):
+        evt = event.name
+
         if evt in ['sys_open', 'sys_openat']:
             filename = event['filename']
         elif evt in ['sys_accept', 'sys_socket']:
@@ -116,10 +140,7 @@ class FDInfo():
             else:
                 filename = ''
 
-        time = ns_to_hour_nsec(event.timestamp)
-        
-        if filename.startswith(self.prefix):
-            print(pid, comm, evt, filename, time)
+        return filename
 
     def output_close(self, event):
         pid = self.cpus[event['cpu_id']].current_tid
@@ -141,6 +162,55 @@ class FDInfo():
         if filename.startswith(self.prefix):
             print(pid, comm, evt, filename, time)
 
+    def output_read(self, event):
+        pid = self.cpus[event['cpu_id']].current_tid
+        if(self.pid >= 0 and self.pid != pid):
+            return
+
+        comm = self.tids[pid].comm
+        if self.pname is not None and self.pname != comm:
+            return
+
+        evt = event.name
+        fds = self.tids[pid].fds
+        
+        if evt == 'sys_splice':
+            fd = event['fd_in']
+        elif evt == 'sys_sendfile64':
+            fd = event['in_fd']
+        else:
+            fd = event['fd']
+
+        if not fd in fds.keys():
+            return
+
+        filename = fds[fd].filename
+        time = ns_to_hour_nsec(event.timestamp)
+
+        if filename.startswith(self.prefix):
+            print(pid, comm, evt, filename, time)
+
+    def output_write(self, event):
+        pid = self.cpus[event['cpu_id']].current_tid
+        if(self.pid >= 0 and self.pid != pid):
+            return
+
+        comm = self.tids[pid].comm
+        if self.pname is not None and self.pname != comm:
+            return
+
+        evt = event.name
+        fds = self.tids[pid].fds
+        fd = event['fd']
+        if not fd in fds.keys():
+            return
+        filename = fds[fd].filename
+        time = ns_to_hour_nsec(event.timestamp)
+
+        if filename.startswith(self.prefix):
+            print(pid, comm, evt, filename, time)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FD syscalls analysis')
     parser.add_argument('path', metavar='<path/to/trace>', help='Trace path')
@@ -148,7 +218,7 @@ if __name__ == '__main__':
                         help='Prefix in which to search')
     parser.add_argument('-t', '--type', type=str, default='all',
                         help='Types of events to display. Possible values:\
-                        all, open, close, dump')
+                        all, open, close, read, write dump')
     parser.add_argument('--pid', type=int, default='-1',
                         help='PID for which to display events')
     parser.add_argument('--pname', type=str, default=None,
@@ -158,7 +228,7 @@ if __name__ == '__main__':
 
     types = args.type.split(',')
 
-    possibleTypes = ['open', 'close', 'dump']
+    possibleTypes = ['open', 'close', 'read', 'write', 'dump']
 
     if 'all' in types:
         isOutputEnabled = { x: True for x in possibleTypes }
