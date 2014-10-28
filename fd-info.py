6de42d77ce2cea7a827d0077aa37ed0e51c67329
Julien Desfossez <jdesfossez@efficios.com>
2014-10-28 14:54:55 -0400

    fix pyflakes/pep8 before stgraber notices
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- fd-info.py
+++ fd-info.py
@@ -13,7 +13,7 @@
 import argparse
 import errno
 import json
-import os.path
+from os.path import join
 import socket
 import sys
 from collections import OrderedDict
@@ -24,7 +24,7 @@ except ImportError:
     sys.path.append("/usr/local/lib/python%d.%d/site-packages" %
                    (sys.version_info.major, sys.version_info.minor))
     from babeltrace import TraceCollection
-from LTTngAnalyzes.common import FDType, ns_to_hour_nsec, NSEC_PER_SEC, MSEC_PER_NSEC
+from LTTngAnalyzes.common import FDType, ns_to_hour_nsec, NSEC_PER_SEC
 from LTTngAnalyzes.sched import Sched
 from LTTngAnalyzes.statedump import Statedump
 from LTTngAnalyzes.syscalls import Syscalls, IOCategory
@@ -32,7 +32,7 @@ try:
     from pymongo import MongoClient
     from pymongo.errors import CollectionInvalid
 except ImportError:
-    nomongolibs = 1
+    nomongolib = 1
 
 NS_IN_S = 1000000000
 NS_IN_MS = 1000000
@@ -183,11 +183,13 @@ class FDInfo():
         sock = None
         try:
             sock = socket.create_connection((self.args.graphite_host,
-                self.args.graphite_port))
+                                             self.args.graphite_port))
         except:
-            print("Couldn't connect to %(server)s on port %(port)d, is " \
-                  "carbon-agent.py running?" % { 'server':self.args.graphite_host,
-                          'port':self.args.graphite_port })
+            print("Couldn't connect to %(server)s on port %(port)d, is "
+                  "carbon-agent.py running?" % {'server':
+                                                self.args.graphite_host,
+                                                'port':
+                                                self.args.graphite_port})
             sys.exit(1)
 
         lines = []
@@ -195,14 +197,14 @@ class FDInfo():
             ts = event["ts_start"]/NSEC_PER_SEC
             if event["category"] == IOCategory.write:
                 l = "hosts.test.latencies.write %d %lu" % \
-                        (event["duration"]/(NSEC_PER_SEC), ts)
+                    (event["duration"]/(NSEC_PER_SEC), ts)
             elif event["category"] == IOCategory.read:
                 l = "hosts.test.latencies.read %d %lu" % \
-                        (event["duration"]/(NSEC_PER_SEC), ts)
+                    (event["duration"]/(NSEC_PER_SEC), ts)
             else:
                 continue
             lines.append(l)
-        message = '\n'.join(lines) + '\n' #all lines must end in a newline
+        message = '\n'.join(lines) + '\n'  # all lines must end in a newline
         sock.sendall(message.encode())
 
     def store_localfile(self):
@@ -211,7 +213,6 @@ class FDInfo():
         nb_write = 0
         total_write = 0
         for event in self.fd_events:
-            ts = event["ts_start"]/NSEC_PER_SEC
             if event["category"] == IOCategory.write:
                 total_write += event["duration"]
                 nb_write += 1
@@ -220,11 +221,11 @@ class FDInfo():
                 nb_read += 1
             else:
                 continue
-        f = open(os.path.join(args.localfile, "avg_w_latency"), "w")
+        f = open(join(args.localfile, "avg_w_latency"), "w")
         f.write("%f\n" % ((total_write/nb_write) / (NSEC_PER_SEC / 1000000)))
         f.close()
 
-        f = open(os.path.join(args.localfile, "avg_r_latency"), "w")
+        f = open(join(args.localfile, "avg_r_latency"), "w")
         f.write("%f\n" % ((total_read/nb_read) / (NSEC_PER_SEC / 1000000)))
         f.close()
 
@@ -353,7 +354,8 @@ class FDInfo():
 
         duration = duration_ns / NS_IN_S
 
-        if self.args.json or self.args.mongo or self.args.graphite or self.args.localfile:
+        if self.args.json or self.args.mongo or self.args.graphite or \
+                self.args.localfile:
             self.log_fd_event_json(tid, pid, comm, entry, name, duration_ns,
                                    filename, ret)
 
@@ -513,10 +515,11 @@ if __name__ == '__main__':
                         help='Store FD events into MongoDB at specified ip\
                         and port')
     parser.add_argument('--graphite', type=str, default=None,
-            help='Store FD events into graphite at specified ip\
+                        help='Store FD events into graphite at specified ip\
                     and port')
     parser.add_argument('--localfile', type=str, default=None,
-            help='Store average latencies in local files (one file per metric)')
+                        help='Store average latencies in local files '
+                        '(one file per metric)')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='Don\'t output fd events to stdout')
 
