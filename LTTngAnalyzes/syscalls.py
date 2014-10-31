9512be1c13d16a5b8e0612595743b637b61cdd34
Julien Desfossez <jdesfossez@efficios.com>
2014-10-31 13:46:45 -0400

    add support for the pid context if present
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- LTTngAnalyzes/syscalls.py
+++ LTTngAnalyzes/syscalls.py
@@ -206,6 +206,13 @@ class Syscalls():
         if c.current_tid == -1:
             return
         t = self.tids[c.current_tid]
+        # check if we can fix the pid from a context
+        if t.pid == -1 and "pid" in event.keys():
+            t.pid = event["pid"]
+            p = Process()
+            p.pid = t.pid
+            p.comm = t.comm
+            self.tids[p.pid] = p
         # if it's a thread, we want the parent
         if t.pid != -1 and t.tid != t.pid:
             t = self.tids[t.pid]
