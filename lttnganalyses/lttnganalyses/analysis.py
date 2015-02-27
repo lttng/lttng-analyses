b88c1bbca02009356908039a6a8899aafa69b80c
Antoine Busque <antoinebusque@gmail.com>
2015-02-26 23:13:16 -0500

    Add callback registration to analysis module

--- lttnganalyses/lttnganalyses/analysis.py
+++ lttnganalyses/lttnganalyses/analysis.py
@@ -26,3 +26,19 @@
 class Analysis:
     def process_event(self, ev):
         raise NotImplementedError()
+
+    def _register_cbs(self, cbs):
+        self._cbs = cbs
+
+    def _process_event_cb(self, ev):
+        name = ev.name
+
+        if name in self._cbs:
+            self._cbs[name](ev)
+        elif "syscall_entry" in self._cbs and \
+           (name.startswith("sys_") or name.startswith("syscall_entry_")):
+            self._cbs["syscall_entry"](ev)
+        elif "syscall_exit" in self._cbs and \
+                (name.startswith("exit_syscall") or
+                 name.startswith("syscall_exit_")):
+            self._cbs["syscall_exit"](ev)
