84d21769303dc52f6e160f33365202e475c9a747
Julien Desfossez <jdesfossez@efficios.com>
2015-03-03 20:04:07 -0500

    new tool to filter a trace based on TID/Procname with follow-child support
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@ cd lttnganalysescli
 cd ..
 
 echo
-echo -n "Install lttng-analyses-record in /usr/local/bin/ ? [Y/n] "
+echo -n "Install lttng-analyses-record and lttng-track-process in /usr/local/bin/ ? [Y/n] "
 read -n 1 a
 echo
 if test "$a" = 'y' -o "$a" = 'Y' -o "$a" = ''; then
 	install lttng-analyses-record /usr/local/bin/lttng-analyses-record
+	install lttng-track-process /usr/local/bin/lttng-track-process
 fi
