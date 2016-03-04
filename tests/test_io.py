66fe8e473b36fd56a3c665f336a883424edd2c16
Julien Desfossez <jdesfossez@efficios.com>
2016-03-04 13:09:17 -0500

    Fix: test lines too long
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- tests/test_io.py
+++ tests/test_io.py
@@ -34,28 +34,34 @@ class IoTest(AnalysisTest):
         self.trace_writer.write_lttng_statedump_file_descriptor(
             1001, 0, 100, 3, 0, 0, 'testfile')
         # app write 10 bytes to FD 4
-        self.trace_writer.write_sched_switch(1002, 0, 'swapper/0', 0, 'app', 99)
+        self.trace_writer.write_sched_switch(1002, 0, 'swapper/0', 0, 'app',
+                                             99)
         self.trace_writer.write_syscall_write(1004, 0, 1, 4, 0xabcd, 10, 10)
         # app2 reads 100 bytes in FD 3
         self.trace_writer.write_sched_switch(1006, 0, 'app', 99, 'app2', 100)
         self.trace_writer.write_syscall_read(1008, 0, 1, 3, 0xcafe, 100, 100)
-        # app3 and its FD 3 are completely unknown at statedump, tries to read 100
-        # bytes from FD 3 but only gets 42
+        # app3 and its FD 3 are completely unknown at statedump, tries to read
+        # 100 bytes from FD 3 but only gets 42
         self.trace_writer.write_sched_switch(1010, 0, 'app2', 100, 'app3', 101)
         self.trace_writer.write_syscall_read(1012, 0, 1, 3, 0xcafe, 100, 42)
         # block write
-        self.trace_writer.write_block_rq_issue(1015, 0, 264241152, 33, 10, 40, 99, 0, 0, '', 'app')
-        self.trace_writer.write_block_rq_complete(1016, 0, 264241152, 33, 10, 0, 0, 0, '')
+        self.trace_writer.write_block_rq_issue(1015, 0, 264241152, 33, 10, 40,
+                                               99, 0, 0, '', 'app')
+        self.trace_writer.write_block_rq_complete(1016, 0, 264241152, 33, 10,
+                                                  0, 0, 0, '')
         # block read
-        self.trace_writer.write_block_rq_issue(1017, 0, 8388608, 33, 20, 90, 101, 1, 0, '', 'app3')
-        self.trace_writer.write_block_rq_complete(1018, 0, 8388608, 33, 20, 0, 1, 0, '')
+        self.trace_writer.write_block_rq_issue(1017, 0, 8388608, 33, 20, 90,
+                                               101, 1, 0, '', 'app3')
+        self.trace_writer.write_block_rq_complete(1018, 0, 8388608, 33, 20, 0,
+                                                  1, 0, '')
         # net xmit
         self.trace_writer.write_net_dev_xmit(1020, 2, 0xff, 32, 100, 'wlan0')
         # net receive
         self.trace_writer.write_netif_receive_skb(1021, 1, 0xff, 100, 'wlan1')
         self.trace_writer.write_netif_receive_skb(1022, 1, 0xff, 200, 'wlan0')
         # syscall open
-        self.trace_writer.write_syscall_open(1023, 0, 1, 'test/open/file', 0, 0, 42)
+        self.trace_writer.write_syscall_open(1023, 0, 1, 'test/open/file', 0,
+                                             0, 42)
         self.trace_writer.flush()
 
     def test_iousagetop(self):
